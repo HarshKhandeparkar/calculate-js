a5c53896055f89bbb08055790a5da9bd0e0e9a8d
HarshKhandeparkar <mjkgoa@gmail.com>
2020-10-21 02:03:59 +0530

    feat: recurrence

--- src/calculate/fraction.ts
+++ src/calculate/fraction.ts
@@ -8,9 +8,19 @@ export function calculateFraction(
   num: number,
   den: number,
   numDig: number
-): string  {
+): {
+  answer: string,
+  recurrence: {
+    isRecurring: boolean,
+    recurrencePosition: number
+  }
+}  {
   let ans = '';
   let last = num; // The last numerator
+  let recurring = false; // Whether the number has started recurring
+  let recurrencePosition: number = -1;
+
+    const lastNumerators: number[] = [ last ]; // A list of numerators that were generated in between, to check for recurrence
 
   /**
    * num/den (num < den)
@@ -22,13 +32,30 @@ export function calculateFraction(
     ans += Math.floor(10*last/den); // Remove the largest multiple of n2 and separate it.
 
     last = (10*last)%den; // The left over numerator
+
+    if (!recurring) {
+      if (lastNumerators.indexOf(last) !== -1) {
+        recurrencePosition = i;
+        recurring = true;
+      }
+      else lastNumerators.push(last);
+    }
   }
 
   // Insert decimal point .
   const decimalIndex = num/den > 1 ? (Math.floor(num/den).toString().length) : 0;
   ans = ans.slice(0, decimalIndex) + '.' + ans.slice(decimalIndex);
 
-  if (ans[0] == '.') ans = '0' + ans;
+  if (ans[0] == '.') {
+    ans = '0' + ans;
+    recurrencePosition++;
+  }
 
-  return ans;
+  return {
+    answer: ans,
+    recurrence: {
+      isRecurring: recurring,
+      recurrencePosition
+    }
+  }
 }
