642d90ecdce45972ceb01ffd26012fb360881cf4
HarshKhandeparkar <shadowwarriorpro3003@gmail.com>
2021-04-19 01:23:01 +0530

    fix: parse outputs positive if number is 0

--- src/util/parse.ts
+++ src/util/parse.ts
@@ -4,9 +4,10 @@ import { validateAndNormalizeNumberString } from './validate-and-normalize';
 export function parse(number: string): IParsedNumber {
   const num = validateAndNormalizeNumberString(number);
 
-  const isNegative = num.startsWith('-');
   const fractionalPart = num.includes('.') ? num.split('.')[1] : '';
   const integralPart = num.includes('.') ? num.split('.')[0] : num;
+
+  const isNegative = num.startsWith('-') && (fractionalPart !== '' || integralPart !== '');
 
   return {
     isNegative,
